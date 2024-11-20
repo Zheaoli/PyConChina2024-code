92baa15193eedfcfb12e5ee0b45fb703b429c380
Manjusaka <me@manjusaka.me>
2024-11-21 02:38:28 +0800

    add test_basic_jit
    Signed-off-by: Manjusaka <me@manjusaka.me>

--- tests/test_jit.py
+++ tests/test_jit.py
@@ -41,3 +41,26 @@ def test_div():
     compiler = AstCompiler(source_code)
     func = compiler.compile()
     assert func(6, 2) == 3
+
+
+def test_basic_jit():
+    """
+    push rbp          ; 55
+    mov rbp, rsp      ; 48 89 e5
+    mov eax, edi      ; 89 f8
+    add eax, esi      ; 01 f0
+    pop rbp           ; 5d
+    ret               ; c3
+    """
+    import mmap
+
+    from cffi import FFI
+
+    byte = bytearray(b"\x55\x48\x89\xe5\x48\x89\xf8\x48\x01\xf0\x5d\xc3")
+    buffer = mmap.mmap(-1, len(byte), mmap.MAP_PRIVATE, mmap.PROT_READ | mmap.PROT_WRITE | mmap.PROT_EXEC)
+    buffer[: len(byte)] = byte
+    ffi = FFI()
+
+    ffi.cdef("typedef int (*fn2)(int, int);")
+    fn = ffi.cast("fn2", ffi.from_buffer(buffer))
+    assert fn(1, 2) == 3
